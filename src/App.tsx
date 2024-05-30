a5f65a99cd3e971241a946123595e5b2d3aff09d
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-05-30 19:55:12 +0200

    add custome colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 function App() {
   return (
     <>
-      <h1 className="font-fraunces text-3xl font-bold ">Hello world!</h1>
-      <h1 className="font-barlow text-3xl font-bold ">Hello world!</h1>
+      <h1 className=" text-dark_cyan text-3xl font-bold ">Hello world!</h1>
+      <h1 className=" text-grey  text-3xl font-bold ">Hello world!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+        non, debitis quis distinctio tenetur aut? Fugiat deleniti vel dolorem
+        velit repudiandae enim sint nostrum nemo fugit ipsa sequi praesentium
+        dolores non error sapiente explicabo, vitae, officia vero, minus atque
+        ut id harum rem? Nisi at est molestiae hic ipsum sint in doloremque
+        voluptas, consequatur cum ipsam, inventore placeat nihil iusto quisquam
+        qui ea illum quaerat suscipit. In, odio laudantium. Provident rem at
+        corrupti similique minus dolore hic nobis voluptates, quas officiis
+        adipisci deserunt consequatur fuga, asperiores magnam illum doloribus
+        odio aliquid assumenda repellat fugiat voluptate culpa? Ratione a maxime
+        amet.
+      </p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         fraunces: ["Fraunces", "serif"],
         barlow: ["Barlow", "sans-serif"],
       },
+      colors: {
+        pale_orange: "#FDD6BA",
+        dark_grey: "#333D4B",
+        grey: "#83888F",
+        dark_cyan: "#0E8784",
+        light_cream: "#FEFCF7",
+      },
     },
   },
   plugins: [],
